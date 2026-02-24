64da3de54030217dbbf63dfa2245f1933d6a1856
CapybaraClickerJS <joaolucasolimpiorodrigues@gmail.com>
2026-02-24 17:07:16 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "vao-infinito-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./three.min.js",
+  "./roblox-oof.mp3"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
